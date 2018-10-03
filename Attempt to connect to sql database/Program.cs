6ba79acb62a6ec16037c411dceecd9e899dcbfc9
Alex Peterson <e44alex@gmail.com>
2018-10-03 21:22:25 +0300

    Initial Commit

---
+++ Attempt to connect to sql database/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient ;
+
+namespace Attempt_to_connect_to_sql_database
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection connection = new SqlConnection("Server=.\\SQLEXPRESS;Database=TEST;Integrated Security=true");
+            connection.Open();
+            SqlCommand command = new SqlCommand("Select * from Users", connection);
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
